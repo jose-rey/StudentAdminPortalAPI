2b2969a21b5165433dac6c9d6a0a2cd7a13bab0f
Jose Rey <rey_gogen86@hotmail.com>
2023-04-03 22:50:51 -0600

    End of Section 2 - API

---
+++ StudentAdminPortalAPI/StudentAdminPortalAPI/Repositories/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using StudentAdminPortalAPI.DataModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortalAPI.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task<List<Student>> GetStudentsAsync();
+    }
+}
---
+++ StudentAdminPortalAPI/StudentAdminPortalAPI/Repositories/SqlStudentRepository.cs
@@ -0,0 +1,26 @@
+﻿using StudentAdminPortalAPI.DataModels;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortalAPI.Repositories
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        private readonly StudentAdminContext context;
+
+        public SqlStudentRepository(StudentAdminContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Student>> GetStudentsAsync()
+        {
+            return await context.Student
+                .Include(nameof(Gender))
+                .Include(nameof(Address))
+                .ToListAsync();
+        }
+    }
+}
